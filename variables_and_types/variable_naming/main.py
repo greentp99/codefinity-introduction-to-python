4940261b061b7a6497723c807a71ebb6dc95dc79
greentp99 <>
2025-11-11 18:50:43 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,14 @@
+
+item = "Bread"
+price = 4.52
+quantity = 230
+# Testing
+print("Item:", item)
+print("Price per loaf: $", price)
+print("Quantity in stock:", quantity)
+
+
+# Testing
+#print("Item:", "Bread")
+#print("Price per loaf: $", 3.89)
+#print("Quantity in stock:", 10)
